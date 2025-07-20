09121ffbd192f6b8ad3e3bd74b53c1ea90c507a8
raksha-git30 <rakshashrimanwar811@gmail.com>
2025-07-20 17:58:14 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log('Server running on http://localhost:3001');
+});
